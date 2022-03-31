fbcfb889d0c4feeb8fdbe3feac89c34d66316dad
macbeth <layne.wegenast@gmail.com>
2022-03-30 21:26:54 -0500

    created index and page-template

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
-const generatePage = (userName, githubName) => `Name: ${userName}, Github: ${githubName}`;
-console.log(generatePage());
+const fs = require('fs');
+
+const generatePage = require('./src/page-template.js');
+
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
