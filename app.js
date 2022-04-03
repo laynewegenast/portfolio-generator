cb366bd07dee7b26322a819558c67c3edc74fee8
macbeth <layne.wegenast@gmail.com>
2022-04-02 23:09:19 -0500

    html updates

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
 // const pageHTML = generatePage(name, github);
 
@@ -112,5 +112,11 @@ const promptProject = portfolioData => {
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
+   const pageHTML = generatePage(portfolioData);
+
+fs.writeFile('./index.html', pageHTML, err => {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
   });
